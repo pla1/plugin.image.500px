854f00ee518acb4cc94c5c48e58e42acfa588984
cykl <clement@unportant.info>
2013-05-21 23:08:23 +0200

    Allow to browse by category.

--- default.py
+++ default.py
@@ -24,23 +24,19 @@ class Image(object):
 
 
 def feature():
-    def get_page(params):
-        try:
-            return int(params['page'])
-        except KeyError:
-            return 1
-
     params = utils.xbmc.addon_params
     feature = params['feature']
-    page = get_page(params)
-    resp = API.photos(feature=feature, rpp=_RPP, consumer_key=_CONSUMER_KEY, image_size=[2, 4], page=page)
+    category = params.get('category', None)
+    page = int(params.get('page', 1))
+
+    resp = API.photos(feature=feature, only=category, rpp=_RPP, consumer_key=_CONSUMER_KEY, image_size=[2, 4], page=page)
 
     for image in map(Image, resp['photos']):
         utils.xbmc.add_image(image)
 
     if resp['current_page'] != resp['total_pages']:
         next_page = page + 1
-        url = utils.xbmc.encode_child_url('feature', feature=feature, page=next_page)
+        url = utils.xbmc.encode_child_url('feature', feature=feature, category=category, page=next_page)
         utils.xbmc.add_dir('Next page', url)
 
     utils.xbmc.end_of_directory()
@@ -73,7 +69,7 @@ def search():
     utils.xbmc.end_of_directory()
 
 
-def root():
+def features():
     features = (
         "editors",
         "popular",
@@ -83,7 +79,7 @@ def root():
     )
 
     for feature in features:
-        url = utils.xbmc.encode_child_url('feature', feature=feature)
+        url = utils.xbmc.encode_child_url('categories', feature=feature)
         utils.xbmc.add_dir(feature, url)
 
     url = utils.xbmc.encode_child_url('search')
@@ -92,9 +88,55 @@ def root():
     utils.xbmc.end_of_directory()
 
 
+def categories():
+    categories = {
+        'Uncategorized': 0,
+        'Abstract': 10,
+        'Animals': 11,
+        'Black and White': 5,
+        'Celebrities': 1,
+        'City': 9,
+        'Commercial': 15,
+        'Concert': 16,
+        'Family': 20,
+        'Fashion': 14,
+        'Film': 2,
+        'Fine Art': 24,
+        'Food': 23,
+        'Journalism': 3,
+        'Landscapes': 8,
+        'Macro': 12,
+        'Nature': 18,
+        'Nude': 4,
+        'People': 7,
+        'Performing Arts': 19,
+        'Sport': 17,
+        'Still Life': 6,
+        'Street': 21,
+        'Transportation': 26,
+        'Travel': 13,
+        'Underwater': 22,
+        'Urban': 27,
+        'Wedding': 25,
+    }
+
+    params = utils.xbmc.addon_params
+    feature = params['feature']
+
+    url = utils.xbmc.encode_child_url('feature', feature=feature)
+    utils.xbmc.add_dir('All', url)
+
+    for category in sorted(categories):
+        url = utils.xbmc.encode_child_url('feature', feature=feature, category=category)
+        utils.xbmc.add_dir(category, url)
+
+    utils.xbmc.end_of_directory()
+
+
 try:
     modes = {
         'feature': feature,
+        'categories': categories,
         'search': search,
     }
 
@@ -102,4 +144,4 @@ try:
     mode_name = params['mode']
     modes[mode_name]()
 except KeyError:
-    root()
+    features()

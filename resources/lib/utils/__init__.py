18f377ed7b357f8b8bccef37d3186a4b866f072c
Patrick L Archibald <patrick.archibald@gmail.com>
2013-06-21 20:19:10 -0400

    Changed utils to fivehundredpxutils

--- resources/lib/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-def print_ret_val(f):
-    def decorated(*args, **kwargs):
-        ret = f(*args, **kwargs)
-        print "%s returned value %s" % (f.__name__, ret)
-        return ret
-
-    return decorated
